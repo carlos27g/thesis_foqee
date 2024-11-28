85fb5478c5fcf33f0180deb0c3fbdea5848b3ae2
Carlos Garzon Rodriguez <carlos.garzon@tum.de>
2024-11-28 15:52:09 +0000

    Fix pylint in main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from termcolor import colored 
+"""
+Main script for generating checklists, this triggers the whole system.
+
+This script performs the following steps:
+1. Creates the input data by calling the `create_input` function.
+2. Identifies the different work products from the input data.
+3. Generates checklists for each identified work product by calling the `generate_checklists` 
+   function.
+
+
+Modules:
+    time: Provides various time-related functions.
+    modules.create_input: Contains the `create_input` function to generate input data.
+    modules.gen_checklist: Contains the `generate_checklists` function to generate checklists.
+    termcolor: Provides functions to print colored text in the terminal.
+
+Execution:
+    This script is intended to be run as a standalone program.
+
+"""
+import time
+
+from termcolor import colored
 
 from modules.create_input import create_input
 from modules.gen_checklist import generate_checklists
-
-import time
 
 
 start_time = time.time()
